ea30e0d724c2628f6bef9fca194130a2fd83c587
Grant Glouser <gglouser@gmail.com>
2019-03-20 11:12:12 -0700

    Tidy use statements (Rust 2018 edition change).

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,11 +1,5 @@
-#[macro_use]
-extern crate cfg_if;
-
-extern crate wasm_bindgen;
+use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
-
-extern crate num_traits;
-extern crate vectors;
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
